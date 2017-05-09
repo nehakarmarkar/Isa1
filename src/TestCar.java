eb3f3f8c0ab0f4f4f0036cf6a4219ffb9f08fe51
nehakarmarkar <nehasarega@gmail.com>
2017-05-09 12:33:12 +0530

    secong commit
    Signed-off-by: nehakarmarkar <nehasarega@gmail.com>

--- src/TestCar.java
+++ src/TestCar.java
@@ -5,10 +5,16 @@ import org.junit.Test;
 public class TestCar {
 
 	@Test
-	public void test() {
+	public void testForFail() {
 		Car r=new Car();
 		r.setCapacity(60);
 		assertEquals(60,r.getEngineCapacity());
+	}
+	
+	public void testForSuccess() {
+		Car r=new Car();
+		r.setCapacity(100);
+		assertEquals(100,r.getEngineCapacity());
 	}
 
 }
